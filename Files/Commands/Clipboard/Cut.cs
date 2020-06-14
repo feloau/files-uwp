a12d98e053ddecaf0cddd4e9e546a89c85f622c1
Yair Aichenbaum <39923744+yaichenbaum@users.noreply.github.com>
2020-06-14 17:27:02 -0400

    Started work on commands

---
+++ Files/Commands/Clipboard/Cut.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Storage;
+
+namespace Files.Commands.Clipboard
+{
+    internal class Cut
+    {
+        public async void CutCommand(string ItemPath)
+        {
+            try
+            {
+                StorageFolder folder = await StorageFolder.GetFolderFromPathAsync(ItemPath);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
